155457d6f9304df22f84b1d5891cc0ad0af2d77d
olamurm <olamykh@gmail.com>
2023-07-11 21:07:49 +0300

    update slicks to clients block

--- app.js
+++ app.js
@@ -2,16 +2,28 @@
 // ====================== sliders ====================
 
 $(".clients__cards").slick({
-  infinite: true,
-  slidesToShow: 2,
-  slidesToScroll: 2,
-  responsive: [
-    {
-      breakpoint: 375,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    }
-  ]
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1410,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                arrows: true,
+                autoplay: false
+            }
+        },
+        {
+            breakpoint: 1409,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 2000
+            }
+        },
+    ]
 });
