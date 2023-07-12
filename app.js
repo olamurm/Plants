c55ee16c21c50cb26b16e39b3b1cbc6b4c6cad22
olamurm <olamykh@gmail.com>
2023-07-12 13:55:06 +0300

    added slick to staff block

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 
 // ====================== sliders ====================
+$(".staff__cards").slick({
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 1410,
+            settings: {
+                infinite: false,
+                dots: false,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 1409,
+            settings: {
+                infinite: true,
+                dots: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        },
+    ]
+})
 
 $(".clients__cards").slick({
     infinite: true,
     slidesToShow: 2,
-    slidesToScroll: 1,
+    slidesToScroll: 2,
     responsive: [
         {
             breakpoint: 1410,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 1,
+                slidesToScroll: 2,
                 arrows: true,
                 autoplay: false
             }
@@ -20,9 +49,9 @@ $(".clients__cards").slick({
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
-                arrows: false,
-                autoplay: true,
-                autoplaySpeed: 2000
+                arrows: false
+                // autoplay: true,
+                // autoplaySpeed: 3000
             }
         },
     ]
